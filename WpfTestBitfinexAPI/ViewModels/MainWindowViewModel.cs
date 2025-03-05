866488bddaf6789eff8497cb655f1b008b320df9
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-03-05 20:11:30 +0500

    Небольшие правки названий, Добавлены методы и команды для обработки событий от трейдов и свечей

--- WpfTestBitfinexAPI/ViewModels/MainWindowViewModel.cs
+++ WpfTestBitfinexAPI/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Reflection.Metadata;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using BitfinexAPIConnector;
 using BitfinexAPIConnector.Abstract;
@@ -40,22 +41,31 @@ public class MainWindowViewModel : BaseViewModel
         {
             long intVal;
             if(long.TryParse(value, out intVal))
-            {
                 SetProperty(ref countOfItemsInRequest, intVal);
-            }
         }
     }
 
-    private string pare;
-    private string Pare
+    private string pair;
+    public string Pair
     {
-        get => pare;
+        get => pair;
         set
         {
-            SetProperty(ref pare, value);
+            SetProperty(ref pair, value);
         }
     }
 
+    private int periodInSec;
+    public string PeriodInSec
+    {
+        get => periodInSec.ToString();
+        set
+        {
+            int intVal;
+            if(int.TryParse(value, out intVal))
+                SetProperty(ref periodInSec, intVal);
+        }
+    } 
 
     public DateTimeOffset dateTimeFrom;
     public string DateTimeFrom
@@ -69,7 +79,7 @@ public class MainWindowViewModel : BaseViewModel
         }
     }
 
-    public DateTimeOffset dateTimeTo = DateTimeOffset.UtcNow;
+    public DateTimeOffset dateTimeTo;
     public string DateTimeTo
     {
         get => dateTimeTo.ToString();
@@ -82,29 +92,107 @@ public class MainWindowViewModel : BaseViewModel
     }
 
 
+    #region REST
     private async Task GetCandleREST()
     {
         candleRESTList.Clear();
-        candleRESTList = (await testConnector.GetCandleSeriesAsync(pare, 60, from:dateTimeFrom,to:dateTimeTo, count:countOfItemsInRequest)).ToObservableCollection();
+        candleRESTList = (await testConnector.GetCandleSeriesAsync(pair, periodInSec, from:dateTimeFrom,to:dateTimeTo, count:countOfItemsInRequest)).ToObservableCollection();
         OnPropertyChanged(nameof(candleRESTList));
     }
     private async Task GetTradeREST()
     {
         tradeRESTList.Clear();
-        tradeRESTList = (await testConnector.GetNewTradesAsync(pare, maxCount: (int)countOfItemsInRequest)).ToObservableCollection();
+        tradeRESTList = (await testConnector.GetNewTradesAsync(pair, maxCount: (int)countOfItemsInRequest)).ToObservableCollection();
         OnPropertyChanged(nameof(tradeRESTList));
     }
+    #endregion
+
+    #region WebSocket
+    private void SubscribeCandleWS()
+    {
+        testConnector.SubscribeCandles(pair, periodInSec, dateTimeFrom, dateTimeTo, countOfItemsInRequest);
+    }
+
+    private void SubscribeTradeWS()
+    {
+        testConnector.SubscribeTrades(pair,(int)countOfItemsInRequest);
+    }
+
+
+    private void UnsubscribeCandleWS()
+    {
+        testConnector.UnsubscribeCandles(pair);
+    }
+
+    private void UnsubscribeTradeWS()
+    {
+        testConnector.UnsubscribeTrades(pair);
+    }
+
+    #endregion
+    #region Actions
+    private void CandleHandler(Candle candle)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            if(candleWSList.ToArray().Length >= countOfItemsInRequest)
+            {
+                candleWSList.RemoveAt(0);
+            }
+            candleWSList.Add(candle);
+            OnPropertyChanged(nameof(candleWSList));
+        });
+    }
+
+    private void BuyTradeHandler(Trade trade)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            if(tradeWSList.ToArray().Length >= countOfItemsInRequest)
+            {
+                tradeWSList.RemoveAt(0);
+            }
+            tradeWSList.Add(trade);
+            OnPropertyChanged(nameof(tradeWSList));
+        });
+    }
+    private void SellTradeHandler(Trade trade)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            if(tradeWSList.Count >= countOfItemsInRequest)
+            {
+                tradeWSList.RemoveAt(0);
+            }
+            tradeWSList.Add(trade);
+            OnPropertyChanged(nameof(tradeWSList));
+        });
+    }
+    #endregion
+
+
+
 
     public MainWindowViewModel()
     {
+        dateTimeTo = DateTimeOffset.Now.AddDays(-1); 
         candleRESTList = new ObservableCollection<Candle>();
         candleWSList = new ObservableCollection<Candle>();
         tradeRESTList = new ObservableCollection<Trade>();
         tradeWSList = new ObservableCollection<Trade>();
         GetCandle = new RelayCommand(async execute => await GetCandleREST(), canExecute => true);
         GetTrade = new RelayCommand(async execute => await GetTradeREST(), canExecute => true);
+        SubscribeCandle = new RelayCommand( execute => SubscribeCandleWS(), canExecute => true);
+        SubscribeTrade = new RelayCommand( execute => SubscribeTradeWS(), canExecute => true);
+        UnsubscribeCandle = new RelayCommand( execute => UnsubscribeCandleWS(), canExecute => true);
+        UnsubscribeTrade = new RelayCommand( execute => UnsubscribeTradeWS(), canExecute => true);
 
         this.testConnector = new TestConnector();
+        testConnector.CandleSeriesProcessing += CandleHandler;
+        testConnector.NewSellTrade += SellTradeHandler;
+        testConnector.NewBuyTrade += BuyTradeHandler;
+
+        periodInSec = 60;
     }
 
 }
